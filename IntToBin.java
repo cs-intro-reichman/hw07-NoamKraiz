b706f10ef59941050ce892fd84323fca978fcb79
Noam Kraiz <noamkraiz@MacBook-Air-sl-Noam.local>
2025-12-27 14:20:02 +0200

    Done

--- IntToBin.java
+++ IntToBin.java
@@ -9,6 +9,14 @@ public class IntToBin {
     /** Returns the binary representation of the given integer, as a string of 0's and 1's.  */
     public static String toBinary(int x) {
         //// Replace the following statement with your code
-        return "";
+        if (x ==0) return "0";
+        String ans="";
+        if (x%2 ==0) ans+= "0";
+        else ans+= "1";
+        while (x>=2) {
+            if (x%2 ==0) return ans= toBinary(x /= 2)+ "0";
+            else return ans= toBinary(x /= 2) + "1";
+        }
+        return ans;
     }    
  }
--- Palindrome.java
+++ Palindrome.java
@@ -8,6 +8,11 @@ public class Palindrome {
 	/** Checks if the given string is a palindrome. */
 	public static boolean isPalindrome(String s) {
 		//// Replace the followaing statement with your code
-		return false;
+		int mid = s.length() / 2;
+		for(int i=0; i< mid;){
+			if(s.charAt(i) != s.charAt(s.length()-1-i)) return false;
+			else return isPalindrome(s.substring(i++ , i--));
+		}
+		return true;
     }
 }
